d5b6d1357572d9a92c898a2663c316f3c071e551
kbaraniak <90936580+kbaraniak@users.noreply.github.com>
2022-10-22 00:20:04 +0200

    Update for CLI

--- api.js
+++ api.js
@@ -2,14 +2,11 @@ const LibrusCore = require("./core");
 const LibrusGrades = require("./modules/grades");
 const var_dump = require('var_dump')
 
-const { login, pass } = require("./config");
-
 let core = new LibrusCore();
 let grades = new LibrusGrades();
 
 
 class Librus {
-
   authUsername(login, pass){
     return core.authUsername(login, pass);
   }
@@ -23,12 +20,4 @@ class Librus {
   }
 }
 
-const api = new Librus();
-
-/* Print Grades on Dictionary List */
-
-api.authUsername(login, pass).then(async function (token) {
-  const usrGrades = await api.getGrades(token).then(data => { return data });
-  console.info(usrGrades)
-});
-
+module.exports = Librus;

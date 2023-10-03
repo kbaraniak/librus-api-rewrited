322cd0ad07100ec93153bc4fc9e12202fb1f20a2
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2023-10-03 22:17:32 +0200

    Important Update - enable return from functions

--- api.js
+++ api.js
@@ -47,8 +47,8 @@ class LibrusAPI {
       }
       return res;
     } catch (error) {
-      console.error(error);
-      return false;
+      // console.error(error.response.data);
+      return error.response.data;
     }
   }
 
@@ -56,49 +56,49 @@ class LibrusAPI {
   async getGrades(){
     await this.session.get(`${this.host}/Grades`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesAverages(){
     await this.session.get(`${this.host}/Grades/Averages`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesCategoriesAverages(){
     await this.session.get(`${this.host}/Grades/CategoriesAverages`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesCategories(){
     await this.session.get(`${this.host}/Grades/Categories`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesComments(){
     await this.session.get(`${this.host}/Grades/Comments`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesScales(){
     await this.session.get(`${this.host}/Grades/Scales`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesTypes(){
     await this.session.get(`${this.host}/Grades/Types`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getGradesUnpreparednessPerSemesterAndSubject(){
     await this.session.get(`${this.host}/Grades/UnpreparednessPerSemesterAndSubject`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   /* To-Do TextGrades */
@@ -107,7 +107,7 @@ class LibrusAPI {
   async getPointGrades(){
     await this.session.get(`${this.host}/PointGrades`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: PointGrades] School doesn't support this function")
     })
@@ -115,7 +115,7 @@ class LibrusAPI {
   async getPointGradesCategories(){
     await this.session.get(`${this.host}/PointGrades/Categories`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: PointGradesCategories] School doesn't support this function")
     })
@@ -123,7 +123,7 @@ class LibrusAPI {
   async getPointGradesDictionaries(){
     await this.session.get(`${this.host}/PointGrades/Dictionaries`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: PointGradesDictionaries] School doesn't support this function")
     })
@@ -131,7 +131,7 @@ class LibrusAPI {
   async getPointGradesAverages(){
     await this.session.get(`${this.host}/PointGrades/Averages`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: PointGradesAverages] School doesn't support this function")
     })
@@ -139,7 +139,7 @@ class LibrusAPI {
   async getPointGradesStudentsAverages(){
     await this.session.get(`${this.host}/PointGrades/StudentsAverages`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: PointGradesAverages] School doesn't support this function")
     })
@@ -147,7 +147,7 @@ class LibrusAPI {
   async getPointGradesCategoriesAverages(){
     await this.session.get(`${this.host}/PointGrades/CategoriesAverages`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: PointGradesAverages] School doesn't support this function")
     })
@@ -161,7 +161,7 @@ class LibrusAPI {
   async getBehaviourGradesTypes(){
     await this.session.get(`${this.host}/BehaviourGrades/Types`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch((e) => {
       console.log("[LibrusAPI: BehaviourGradesTypes] School doesn't support this function")
     })
@@ -169,7 +169,7 @@ class LibrusAPI {
   async getBehaviourGradesPoints(){
     await this.session.get(`${this.host}/BehaviourGrades/Points`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: BehaviourGradesPoints] School doesn't support this function")
     })
@@ -177,7 +177,7 @@ class LibrusAPI {
   async getBehaviourGradesPointsCategories(){
     await this.session.get(`${this.host}/BehaviourGrades/Points/Categories`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: BehaviourGradesPointsCategories] School doesn't support this function")
     })
@@ -185,7 +185,7 @@ class LibrusAPI {
   async getBehaviourGradesPointsComments(){
     await this.session.get(`${this.host}/BehaviourGrades/Points/Comments`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: BehaviourGradesPointsCategories] School doesn't support this function")
     })
@@ -193,7 +193,7 @@ class LibrusAPI {
   async getBehaviourGradesSystemProposal(){
     await this.session.get(`${this.host}/BehaviourGrades/SystemProposal`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: BehaviourGradesSystemProposal] School doesn't support this function")
     })
@@ -203,31 +203,31 @@ class LibrusAPI {
   async getLessons(){
     await this.session.get(`${this.host}/Lessons`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getAttendances(){
     await this.session.get(`${this.host}/Attendances`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getAttendancesTypes(){
     await this.session.get(`${this.host}/Attendances/Types`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getAttendancesLessonsStatistics(id){
     await this.session.get(`${this.host}/Attendances/LessonsStatistics/${id}`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     })
   }
   async getAttendancesFilledByTeacher(id=""){
     await this.session.get(`${this.host}/Attendances/FilledByTeacher/${id}`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: AttendancesFilledByTeacher] School doesn't support this function")
     })
@@ -235,7 +235,7 @@ class LibrusAPI {
   async getTimetables(){
     await this.session.get(`${this.host}/Timetables`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Timetables] School doesn't support this function")
     })
@@ -243,7 +243,7 @@ class LibrusAPI {
   async getTimetablesDate(arg){
     await this.session.get(`${this.host}/Timetables?${arg}`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: TimetablesDate] School doesn't support this function")
     })
@@ -269,7 +269,7 @@ class LibrusAPI {
   async getSubstitutions(){
     await this.session.get(`${this.host}/Substitutions`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Substitutions] School doesn't support this function")
     })
@@ -277,7 +277,7 @@ class LibrusAPI {
   async getSubjects(){
     await this.session.get(`${this.host}/Subjects`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Subjects] School doesn't support this function")
     })
@@ -285,7 +285,7 @@ class LibrusAPI {
   async getTeacherFreeDays(){
     await this.session.get(`${this.host}/TeacherFreeDays`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: TeacherFreeDays] School doesn't support this function")
     })
@@ -293,7 +293,7 @@ class LibrusAPI {
   async getLuckyNumber(){
     await this.session.get(`${this.host}/LuckyNumbers`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: LuckyNumber] School doesn't support this function")
     })
@@ -304,7 +304,7 @@ class LibrusAPI {
   async getSchool(){
     await this.session.get(`${this.host}/Schools`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: School] School doesn't support this function")
     })
@@ -312,7 +312,7 @@ class LibrusAPI {
   async getClassrooms(){
     await this.session.get(`${this.host}/Classrooms`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Classrooms] School doesn't support this function")
     })
@@ -320,7 +320,7 @@ class LibrusAPI {
   async getVirtualClasses(){
     await this.session.get(`${this.host}/VirtualClasses`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: VirtualClasses] School doesn't support this function")
     })
@@ -328,7 +328,7 @@ class LibrusAPI {
   async getSchoolNotices(){
     await this.session.get(`${this.host}/SchoolNotices`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: SchoolNotices] School doesn't support this function")
     })
@@ -336,7 +336,7 @@ class LibrusAPI {
   async getRealizationsTypesOfDays(){
     await this.session.get(`${this.host}/Realizations/TypesOfDays`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: RealizationsTypesOfDays] School doesn't support this function")
     })
@@ -344,7 +344,7 @@ class LibrusAPI {
   async getRealizationsTypesOfClasses(){
     await this.session.get(`${this.host}/Realizations/TypesOfClasses`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: RealizationsTypesOfClasses] School doesn't support this function")
     })
@@ -354,7 +354,7 @@ class LibrusAPI {
   async getClassFreeDays(){
     await this.session.get(`${this.host}/ClassFreeDays`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: ClassFreeDays] School doesn't support this function")
     })
@@ -362,7 +362,7 @@ class LibrusAPI {
   async getClasses(){
     await this.session.get(`${this.host}/Classes`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Classes] School doesn't support this function")
     })
@@ -370,7 +370,7 @@ class LibrusAPI {
   async getClassesCrossedOutStudents(){
     await this.session.get(`${this.host}/Classes/CrossedOutStudents`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: ClassesCrossedOutStudents] School doesn't support this function")
     })
@@ -380,7 +380,7 @@ class LibrusAPI {
   async getHelp(){
     await this.session.get(`${this.host}/Help`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Help] School doesn't support this function")
     })
@@ -388,7 +388,7 @@ class LibrusAPI {
   async getEndpoints(){
     await this.session.get(`${this.host}/Root`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Root] School doesn't support this function")
     })
@@ -396,7 +396,7 @@ class LibrusAPI {
   async getWhatsNew(){
     await this.session.get(`${this.host}/WhatsNew`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: WhatsNew] School doesn't support this function")
     })
@@ -404,7 +404,7 @@ class LibrusAPI {
   async getNotificationCenter(){
     await this.session.get(`${this.host}/NotificationCenter`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: NotificationCenter] School doesn't support this function")
     })
@@ -412,7 +412,7 @@ class LibrusAPI {
   async getMe(){
     await this.session.get(`${this.host}/Me`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Me] School doesn't support this function")
     })
@@ -420,7 +420,7 @@ class LibrusAPI {
   async getColors(){
     await this.session.get(`${this.host}/Colors`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Colors] School doesn't support this function")
     })
@@ -428,7 +428,7 @@ class LibrusAPI {
   async getUsers(){
     await this.session.get(`${this.host}/Users`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch(() => {
       console.log("[LibrusAPI: Users] School doesn't support this function")
     })
@@ -436,7 +436,7 @@ class LibrusAPI {
   async getAPI(endpoint){
     await this.session.get(`${this.host}/${endpoint}`)
     .then((r) => {
-      console.log(r.data)
+      return r.data
     }).catch((e) => {
       console.log("[LibrusAPI: getAPI] Invalid Endpoint: " + endpoint)
     })

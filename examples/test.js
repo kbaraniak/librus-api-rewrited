eccc096539149138371702a7edbaed679c7cfc1e
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2024-09-11 15:08:51 +0000

    Updated test script

--- examples/test.js
+++ examples/test.js
@@ -3,11 +3,15 @@ const { LibrusAPI } = require('../api');
 async function main() {
   try {
     const librusApi = new LibrusAPI();
-    const tokenCreated = await librusApi.mkToken('synergia_login', 'synergia_password');
+    const tokenCreated = await librusApi.mkToken('login_synergia', 'password_synergia');
     console.log('Token Created:', tokenCreated);
     if(tokenCreated){
       librusApi.getLessons()
-      librusApi.getAPI("SystemData")
+      test = await librusApi.getAPI("SystemData")
+      console.log(test)
+      
+      tmpGrades = await librusApi.getGrades();
+      console.log(tmpGrades)
     }
   } catch (error) {
     console.error('Error:', error);
